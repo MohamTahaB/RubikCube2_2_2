7eecbd16178916f4b606bd7ab7e0e3be94bdd1b6
MohamTahaB <94201467+MohamTahaB@users.noreply.github.com>
2022-02-02 01:08:11 +0100

    Movements complete

--- src/Cube.java
+++ src/Cube.java
@@ -42,29 +42,162 @@ public class Cube {
     }
 
     public void rotateFrontAntiClkw() {
-        //first rotation of the face
-        String att = Front[0][0] ;
-        Front[0][0] = Front[0][1] ;
-        Front[0][1] = Front[1][1] ;
-        Front[1][1] = Front[1][0] ;
-        Front[1][0] = att ;
-
-        //now to the rest
-        String a , b ;
-        a = Up[1][0] ;
-        b = Up[1][1] ;
-        Up[1][0] = Right[0][0];
-        Up[1][1] = Right[1][0] ;
-
-        Right[0][0] = Down[0][1] ;
-        Right[1][0] = Down[0][0] ;
-
-        Down[0][1] = Left[1][1];
-        Down[0][0] = Left[0][1];
-
-        Left[1][1] = a;
-        Left[0][1] = b;
+       rotateFrontClkw();
+        rotateFrontClkw();
+        rotateFrontClkw();
     }
 
+
+    public void rotateBackClkw() {
+        String att = Back[0][1] ;
+        Back[0][1] = Back[0][0] ;
+        Back[0][0] = Back[1][0] ;
+        Back[1][0] = Back[1][1] ;
+        Back[1][1] = att ;
+
+        //now to the rest
+        String aux1  = Up[0][1] , aux2 = Up[0][0] ;
+        Up[0][1] = Right[1][1] ;
+        Up[0][0] = Right[0][1] ;
+
+        Right[1][1] = Down[1][0] ;
+        Right[0][1] = Down[1][1] ;
+
+        Down[1][0] = Left[0][0] ;
+        Down[1][1] = Left[1][0] ;
+
+        Left[0][0] = aux1 ;
+        Left[1][0] = aux2 ;
+    }
+
+    public void rotateBackAntiClkw() {
+        rotateBackClkw() ;
+        rotateBackClkw() ;
+        rotateBackClkw() ;
+    }
+
+    public void rotateRightClkw() {
+        // the face first
+        String aux = Right[0][0] ;
+        Right[0][0] = Right[1][0] ;
+        Right[1][0] = Right[1][1] ;
+        Right[1][1] = Right[0][1] ;
+        Right[0][1] = aux ;
+
+        //Now to the rest
+
+        String aux1 = Up[0][1]  , aux2 = Up[1][1] ;
+        Up[0][1] = Front[0][1] ;
+        Up[1][1] = Front[1][1] ;
+
+        Front[0][1] = Down[0][1] ;
+        Front[1][1] = Down[1][1] ;
+
+        Down[0][1] = Back[1][0] ;
+        Down[1][1] = Back[0][0] ;
+
+        Back[1][0] = aux1 ;
+        Back[0][0] = aux2 ;
+    }
+
+    public void rotateRightAntiClkw() {
+        rotateRightClkw() ;
+        rotateRightClkw() ;
+        rotateRightClkw() ;
+    }
+
+    public void rotateLeftClkw(){
+        //the face first
+
+        String aux = Left[0][0] ;
+        Left[0][0] = Left[1][0] ;
+        Left[1][0] = Left[1][1] ;
+        Left[1][1] = Left[0][1] ;
+        Left[0][1] = aux ;
+
+        //now to the rest
+        String aux1 = Up[0][0] , aux2 = Up[1][0] ;
+
+        Up[0][0] = Back[1][1] ;
+        Up[1][0] = Back[0][1] ;
+
+        Back[1][1] = Down[0][0] ;
+        Back[0][1] = Down[1][0] ;
+
+        Down[0][0] = Front[0][0] ;
+        Down[1][0] = Front[1][0] ;
+
+        Front[0][0] = aux1 ;
+        Front[1][0] = aux2 ;
+    }
+
+    public void rotateLeftAntiClkw() {
+        rotateLeftClkw() ;
+        rotateLeftClkw() ;
+        rotateLeftClkw() ;
+    }
+
+    public void rotateUpClkw() {
+        String aux = Up[0][0] ;
+        Up[0][0] = Up[1][0] ;
+        Up[1][0] = Up[1][1] ;
+        Up[1][1] = Up[0][1] ;
+        Up[0][1] = aux ;
+
+        // now the rest
+
+        String aux1 = Back[0][0] , aux2 = Back[0][1] ;
+
+        Back[0][0] = Left[0][0] ;
+        Back[0][1] = Left[0][1] ;
+
+        Left[0][0] = Front[0][0] ;
+        Left[0][1] = Front[0][1] ;
+
+        Front[0][0] = Right[0][0] ;
+        Front[0][1] = Right[0][1] ;
+
+        Right[0][0] = aux1 ;
+        Right[0][1] = aux2 ;
+    }
+
+    public void rotateUpAntiClkw() {
+        rotateUpClkw() ;
+        rotateUpClkw() ;
+        rotateUpClkw() ;
+    }
+
+    public void rotateDownClkw(){
+        //first the face
+
+        String aux = Down[0][0] ;
+        Down[0][0] = Down[1][0] ;
+        Down[1][0] = Down[1][1] ;
+        Down[1][1] = Down[0][1] ;
+        Down[0][1] = aux ;
+
+        //now the rest
+
+        String aux1 = Front[1][0] , aux2 = Front[1][1] ;
+
+        Front[1][0] = Left[1][0] ;
+        Front[1][1] = Left[1][1] ;
+
+        Left[1][0] = Back[1][0] ;
+        Left[1][1] = Back[1][1] ;
+
+        Back[1][0] = Right[1][0] ;
+        Back[1][1] = Right[1][1] ;
+
+        Right[1][0] = aux1 ;
+        Right[1][1] = aux2 ;
+    }
+
+
+    public void rotateDownAntiClkw() {
+        rotateDownClkw() ;
+        rotateDownClkw() ;
+        rotateDownClkw() ;
+    }
 
 }
